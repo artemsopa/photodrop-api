c894a73e872b9c3d1f36f34af36073b205001887
Artom Sopun <artomsopun@gmail.com>
2022-08-26 18:22:36 +0300

    Add es-lint

---
+++ photodrop-api-admin/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'class-methods-use-this': 0,
+    'no-return-await': 0,
+    'max-classes-per-file': 0,
+    'no-unused-vars': 0,
+    'linebreak-style': 0,
+    'lines-between-class-members': 0,
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    'import/no-unresolved': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-param-reassign': 0,
+    'no-extra-semi': 0,
+    semi: 0,
+  },
+};
