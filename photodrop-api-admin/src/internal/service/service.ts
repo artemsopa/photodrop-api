ca174731cfdf9a1c8f2229cf32283e009a5af184
Artom Sopun <artomsopun@gmail.com>
2022-08-28 18:56:01 +0300

    Add user service

--- photodrop-api-admin/src/internal/service/service.ts
+++ photodrop-api-admin/src/internal/service/service.ts
@@ -3,9 +3,11 @@ import { Hasher } from '../../pkg/hasher/password';
 import Repositories from '../repository/repository';
 import { AlbumInfo, AlbumInput } from './dtos/album';
 import { ImageInput, ImageInfo } from './dtos/image';
-import { UserInput } from './dtos/user';
+import { UserInfo, UserInput } from './dtos/user';
 import AuthService from './auth.service';
 import AlbumsService from './albums.service';
+import ImagesService from './images.service';
+import UsersService from './users.service';
 
 export interface IAuthService {
     signUp(user: UserInput): Promise<void>;
@@ -22,6 +24,10 @@ export interface IImagesService {
     create(userId: string, image: ImageInput): Promise<void>;
 }
 
+export interface IUsersService {
+  getAll(): Promise<UserInfo[]>
+}
+
 export class Deps {
   repos: Repositories;
   hasher: Hasher;
@@ -36,8 +42,12 @@ export class Deps {
 export default class Services {
   auth: IAuthService;
   almubs: IAlbumsService;
+  images: IImagesService;
+  users: IUsersService;
   constructor(deps: Deps) {
     this.auth = new AuthService(deps.repos.users, deps.hasher, deps.authManager);
     this.almubs = new AlbumsService(deps.repos.albums);
+    this.images = new ImagesService(deps.repos.images);
+    this.users = new UsersService(deps.repos.users);
   }
 }
