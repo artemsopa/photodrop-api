b411733f3dc7a4a563df3d4d0f36c6c8c36aaf33
Artom Sopun <artomsopun@gmail.com>
2022-08-28 19:00:38 +0300

    Add express error middleware

---
+++ photodrop-api-admin/src/internal/delivery/middlewares/error.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import ApiError from '../../../../domain/error';
+
+const errorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
+  if (error instanceof ApiError) {
+    return res.status(error.status).json({
+      message: error.message,
+      errors: error.errors,
+    });
+  }
+  return res.status(500).json({
+    message: 'Internal server error!',
+  });
+};
+
+export default errorMiddleware;
