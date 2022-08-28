b076c50647cb2d30eb92348d5a847fe1ceb001a7
Artom Sopun <artomsopun@gmail.com>
2022-08-28 17:58:39 +0300

    Update jwt manager

--- photodrop-api-admin/src/pkg/auth/jwt.ts
+++ photodrop-api-admin/src/pkg/auth/jwt.ts
@@ -1,21 +1,21 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-export interface JwtManager {
+export interface AuthManager {
     newToken(userID: string): string;
     verifyToken(token: string): string | null;
 }
 
 export interface JwtPlaceholder {
-    userID: string;
+    userId: string;
 }
 
-export class AuthManager implements JwtManager {
+export class JwtManager implements AuthManager {
   signingKey: string;
   TTL: string;
 
   constructor(signingKey: string, TTL: string) {
     if (!signingKey) {
-      throw new Error('Empty access signing key!')
+      throw new Error('Empty access signing key!');
     }
     this.signingKey = signingKey;
     this.TTL = TTL;
@@ -28,9 +28,9 @@ export class AuthManager implements JwtManager {
   verifyToken(token: string) {
     try {
       const jwtObj = jwt.verify(token, this.signingKey) as JwtPlaceholder;
-      return jwtObj.userID;
+      return jwtObj.userId;
     } catch (error) {
-      return null
+      return null;
     }
   }
 }
