a3592d50a675335e5bed292d7d76511eec5aa888
Artom Sopun <artomsopun@gmail.com>
2022-08-28 21:52:23 +0300

    Delete hasher

--- photodrop-api-admin/src/app/app.ts
+++ photodrop-api-admin/src/app/app.ts
@@ -1,7 +1,6 @@
 import initConfigs from '../configs/config';
 import Repositories from '../internal/repository/repository';
 import Services, { Deps } from '../internal/service/service';
-import { BcryptHasher } from '../pkg/hasher/password';
 import { JwtManager } from '../pkg/auth/jwt';
 import initDB from '../pkg/database/database';
 import Handler from '../internal/delivery/handler';
@@ -10,11 +9,9 @@ export const runApp = () => {
   const configs = initConfigs();
 
   const ds = initDB(configs.db.DB_HOST, configs.db.DB_PORT, configs.db.DB_USER, configs.db.DB_PASSWORD, configs.db.DB_NAME);
-  const hasher = new BcryptHasher(configs.hasher.HASHER_SALT);
   const jwtManager = new JwtManager(configs.jwt.JWT_SIGNING_KEY, configs.jwt.JWT_TTL);
   const deps = new Deps(
     new Repositories(ds),
-    hasher,
     jwtManager,
   );
   const services = new Services(deps);
