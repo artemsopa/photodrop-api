bd83e5e862a41b0fe2c047cb50294c76b824f790
Artom Sopun <artomsopun@gmail.com>
2022-08-28 21:48:24 +0300

    Delete hasher, sign-up

--- photodrop-api-admin/src/internal/service/service.ts
+++ photodrop-api-admin/src/internal/service/service.ts
@@ -1,16 +1,12 @@
 import { AuthManager } from '../../pkg/auth/jwt';
-import { Hasher } from '../../pkg/hasher/password';
 import Repositories from '../repository/repository';
 import { AlbumInfo, AlbumInput } from './dtos/album';
 import { ImageInput, ImageInfo } from './dtos/image';
-import { UserInfo, UserInput } from './dtos/user';
 import AuthService from './auth.service';
 import AlbumsService from './albums.service';
 import ImagesService from './images.service';
-import UsersService from './users.service';
 
 export interface IAuthService {
-    signUp(user: UserInput): Promise<void>;
     signIn(login: string, password: string): Promise<string>;
 }
 
@@ -24,17 +20,11 @@ export interface IImagesService {
     create(userId: string, image: ImageInput): Promise<void>;
 }
 
-export interface IUsersService {
-  getAll(): Promise<UserInfo[]>
-}
-
 export class Deps {
   repos: Repositories;
-  hasher: Hasher;
   authManager: AuthManager;
-  constructor(repos: Repositories, hasher: Hasher, authManager: AuthManager) {
+  constructor(repos: Repositories, authManager: AuthManager) {
     this.repos = repos;
-    this.hasher = hasher;
     this.authManager = authManager;
   }
 }
@@ -43,11 +33,9 @@ export default class Services {
   auth: IAuthService;
   almubs: IAlbumsService;
   images: IImagesService;
-  users: IUsersService;
   constructor(deps: Deps) {
-    this.auth = new AuthService(deps.repos.users, deps.hasher, deps.authManager);
+    this.auth = new AuthService(deps.repos.users, deps.authManager);
     this.almubs = new AlbumsService(deps.repos.albums);
     this.images = new ImagesService(deps.repos.images);
-    this.users = new UsersService(deps.repos.users);
   }
 }
