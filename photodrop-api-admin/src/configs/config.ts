a6973f8f1fae698eb6df79f168afdd0f7b54c6e3
Artom Sopun <artomsopun@gmail.com>
2022-08-29 00:22:59 +0300

    Delete hasher salt env initialization

--- photodrop-api-admin/src/configs/config.ts
+++ photodrop-api-admin/src/configs/config.ts
@@ -8,21 +8,17 @@ const initConfigs = () => {
     DB_USER,
     DB_PASSWORD,
     DB_NAME,
-    HASHER_SALT,
     JWT_SIGNING_KEY,
     JWT_TTL,
   } = process.env;
 
-  if (!DB_HOST || !DB_PORT || !DB_USER || !DB_PASSWORD || !DB_NAME || !HASHER_SALT || !JWT_SIGNING_KEY || !JWT_TTL) {
+  if (!DB_HOST || !DB_PORT || !DB_USER || !DB_PASSWORD || !DB_NAME || !JWT_SIGNING_KEY || !JWT_TTL) {
     throw new Error('ERROR! Invalid configuration');
   }
 
   return {
     db: {
       DB_HOST, DB_PORT: Number(DB_PORT), DB_USER, DB_PASSWORD, DB_NAME,
-    },
-    hasher: {
-      HASHER_SALT: Number(HASHER_SALT),
     },
     jwt: {
       JWT_SIGNING_KEY,
