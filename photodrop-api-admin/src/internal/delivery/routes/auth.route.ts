24754cd281ce69dfd3b3c14e6154da2478e9322c
Artom Sopun <artomsopun@gmail.com>
2022-08-28 21:51:01 +0300

    Delete registerSchema

--- photodrop-api-admin/src/internal/delivery/routes/auth.route.ts
+++ photodrop-api-admin/src/internal/delivery/routes/auth.route.ts
@@ -2,8 +2,7 @@ import {
   NextFunction, Request, Response, Router,
 } from 'express';
 import { IAuthService } from '../../service/service';
-import { registerSchema, loginSchema } from './joi-schemas/auth.schema';
-import { UserInput } from '../../service/dtos/user';
+import { loginSchema } from './joi-schemas/auth.schema';
 import validateSchema from './joi-schemas/schema';
 
 class AuthRoute {
@@ -13,8 +12,7 @@ class AuthRoute {
 
   initRoutes() {
     return Router()
-      .post('/login', this.login.bind(this))
-      .post('/register', this.register.bind(this));
+      .post('/login', this.login.bind(this));
   }
 
   private async login(req: Request, res: Response, next: NextFunction) {
@@ -22,16 +20,6 @@ class AuthRoute {
       const body = validateSchema(loginSchema, req.body);
       const token = await this.authService.signIn(body.login, body.password);
       res.status(201).json({ token });
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  private async register(req: Request, res: Response, next: NextFunction) {
-    try {
-      const body = validateSchema(registerSchema, req.body);
-      await this.authService.signUp(new UserInput(body.login, body.password, body.fullName, body.email));
-      res.status(201).json({ message: `User ${body.login} successfully registered!` });
     } catch (error) {
       next(error);
     }
--- photodrop-api-admin/src/internal/delivery/routes/joi-schemas/auth.schema.ts
+++ photodrop-api-admin/src/internal/delivery/routes/joi-schemas/auth.schema.ts
@@ -12,25 +12,6 @@ export const loginSchema = Joi.object({
     .required(),
 });
 
-export const registerSchema = Joi.object({
-  login: Joi.string()
-    .min(4)
-    .max(30)
-    .lowercase()
-    .required(),
-  password: Joi.string()
-    .min(6)
-    .max(30)
-    .required(),
-  fullName: Joi.string()
-    .min(6)
-    .max(30),
-  email: Joi.string()
-    .min(6)
-    .max(30)
-    .email(),
-});
-
 export const idSchema = Joi.object().keys({
   userId: Joi.string().required(),
 }).unknown(true);
