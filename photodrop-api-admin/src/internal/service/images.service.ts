82696733fc1475cc139fe760ed18769e750baa63
Artom Sopun <artomsopun@gmail.com>
2022-08-28 18:55:28 +0300

    Update imports, export

--- photodrop-api-admin/src/internal/service/images.service.ts
+++ photodrop-api-admin/src/internal/service/images.service.ts
@@ -1,18 +1,20 @@
 import { ImageInfo, ImageInput } from './dtos/image';
 import { IImagesService } from './service';
-import { IImagesRepo } from './../repository/repository';
+import { IImagesRepo } from '../repository/repository';
 import Image from '../repository/entities/image';
 
-class ImageService implements IImagesService {
+class ImagesService implements IImagesService {
   constructor(private imagesRepo: IImagesRepo) {
     this.imagesRepo = imagesRepo;
   }
 
   async getAll(userId: string, albumId: string): Promise<ImageInfo[]> {
-    return (await this.imagesRepo.getAllByAlbumId(albumId)).map(item => new ImageInfo(item.id, item.path));
+    return (await this.imagesRepo.getAllByAlbumId(albumId)).map((item) => new ImageInfo(item.id, item.path));
   }
 
   async create(userId: string, image: ImageInput): Promise<void> {
     await this.imagesRepo.create(new Image(image.path, image.albumId));
   }
 }
+
+export default ImagesService;
