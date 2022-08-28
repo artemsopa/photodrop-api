92e1e3777965be796c80fded00b66810212048d8
Artom Sopun <artomsopun@gmail.com>
2022-08-28 18:14:45 +0300

    Add error inheritance

---
+++ photodrop-api-admin/src/internal/domain/error.ts
@@ -0,0 +1,14 @@
+export default class ApiError extends Error {
+  status: number;
+  errors: any[];
+
+  constructor(status: number, message: string, errors: any[] = []) {
+    super(message);
+    this.status = status;
+    this.errors = errors;
+  }
+
+  static unauthorized() {
+    return new ApiError(401, 'You are unauthorized!');
+  }
+}
