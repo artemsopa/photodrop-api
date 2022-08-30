0f73ae2092c579648e7fe4ff04f2656789fc4fc2
Artom Sopun <artomsopun@gmail.com>
2022-08-30 08:17:47 +0300

    Add local express start

--- photodrop-serverless-api/src/internal/delivery/handler.ts
+++ photodrop-serverless-api/src/internal/delivery/handler.ts
@@ -16,12 +16,24 @@ class Handler {
   }
 
   initHandler() {
-    return express()
-      .use(cors())
+    // local
+    const app = express();
+    app.use(cors())
       .use(express.json())
       .use(this.initRoutes())
       .use(errorMiddleware)
       .use(notfMiddleware);
+    app.listen(3000, () => {
+      console.log(`Server is running at https://localhost:${3000}`);
+    });
+
+    // prod
+    // return express()
+    //   .use(cors())
+    //   .use(express.json())
+    //   .use(this.initRoutes())
+    //   .use(errorMiddleware)
+    //   .use(notfMiddleware);
   }
 
   private initRoutes() {
