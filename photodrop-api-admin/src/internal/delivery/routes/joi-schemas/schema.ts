7dc08f6265c68e0a80623067a46ed716496d2f6a
Artom Sopun <artomsopun@gmail.com>
2022-09-01 02:48:53 +0300

    Update error message

--- photodrop-api-admin/src/internal/delivery/routes/joi-schemas/schema.ts
+++ photodrop-api-admin/src/internal/delivery/routes/joi-schemas/schema.ts
@@ -3,7 +3,7 @@ import ApiError from '../../../domain/error';
 
 const validateSchema = (schema: Joi.Schema, ctx: any) => {
   const { error, value } = schema.validate(ctx, { abortEarly: false });
-  if (error) throw new ApiError(400, `Bad Request!${error.details[0].message}`);
+  if (error) throw new ApiError(400, `Bad Request! ${error.details[0].message}`);
   return value;
 };
 
