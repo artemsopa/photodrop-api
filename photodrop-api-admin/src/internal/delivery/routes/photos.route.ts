ae14c89e07baeebe8c88a3418430e563585f65e8
Artom Sopun <artomsopun@gmail.com>
2022-09-01 02:50:40 +0300

    Add getAll method

--- photodrop-api-admin/src/internal/delivery/routes/photos.route.ts
+++ photodrop-api-admin/src/internal/delivery/routes/photos.route.ts
@@ -4,7 +4,7 @@ import {
 import { IPhotosService } from '../../service/service';
 import { AuthMiddleware } from '../middlewares/auth.middleware';
 import validateSchema from './joi-schemas/schema';
-import { insertPhotosSchema, getSignedUrlSchema } from './joi-schemas/photo.schema';
+import { insertPhotosSchema, getSignedUrlSchema, idReqSchema } from './joi-schemas/photo.schema';
 
 class PhotosRoute {
   constructor(private photosService: IPhotosService, private authMiddleware: AuthMiddleware) {
@@ -15,7 +15,8 @@ class PhotosRoute {
   initRoutes() {
     return Router()
       .post('/s3url', this.createUploadUrl.bind(this))
-      .post('/', this.createMany.bind(this));
+      .post('/', this.createMany.bind(this))
+      .get('/album', this.getAllByAlbum.bind(this));
   }
 
   private async createUploadUrl(req: Request, res: Response, next: NextFunction) {
@@ -35,6 +36,17 @@ class PhotosRoute {
       const body = validateSchema(insertPhotosSchema, req.body);
       await this.photosService.createMany(cameristId, body.albumId, body.photos);
       res.status(200).json({ message: 'Photos successfully uploaded!' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  private async getAllByAlbum(req: Request, res: Response, next: NextFunction) {
+    try {
+      const cameristId = this.authMiddleware.getCameristId(req);
+      const id = validateSchema(idReqSchema, req.query);
+      const photos = await this.photosService.getAllByAlbum(cameristId, id);
+      res.status(200).json(photos);
     } catch (error) {
       next(error);
     }
