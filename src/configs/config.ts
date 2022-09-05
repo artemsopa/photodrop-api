13563887c77333f61b6388baa772e9d721cd892a
Artom Sopun <artomsopun@gmail.com>
2022-09-05 17:27:48 +0300

    Add otp

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -12,9 +12,23 @@ const initConfigs = () => {
     JWT_TTL,
     S3_REGION,
     S3_BUCKET,
+    TWILIO_SID,
+    TWILIO_TOKEN,
+    TWILIO_SERVICE,
   } = process.env;
 
-  if (!DB_HOST || !DB_PORT || !DB_USER || !DB_PASSWORD || !DB_NAME || !JWT_SIGNING_KEY || !JWT_TTL || !S3_REGION || !S3_BUCKET) {
+  if (!DB_HOST
+    || !DB_PORT
+    || !DB_USER
+    || !DB_PASSWORD
+    || !DB_NAME
+    || !JWT_SIGNING_KEY
+    || !JWT_TTL
+    || !S3_REGION
+    || !S3_BUCKET
+    || !TWILIO_SID
+    || !TWILIO_TOKEN
+    || !TWILIO_SERVICE) {
     throw new Error('ERROR! Invalid configuration');
   }
 
@@ -29,6 +43,11 @@ const initConfigs = () => {
     jwt: {
       JWT_SIGNING_KEY,
       JWT_TTL,
+    },
+    otp: {
+      TWILIO_SID,
+      TWILIO_TOKEN,
+      TWILIO_SERVICE,
     },
   };
 };
