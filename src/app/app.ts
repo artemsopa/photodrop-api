23439de56ddb601fd9a667e9f5e8c45bc3021161
Artom Sopun <artomsopun@gmail.com>
2022-09-06 02:02:51 +0300

    Took configs, data source over the create app function

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,27 @@
-import initConfigs from '../configs/config';
+import { DataSource } from 'typeorm';
+import initConfigs, { Configs } from '../configs/config';
 import Repositories from '../internal/repository/repository';
 import Services, { Deps } from '../internal/service/service';
-import { JwtManager } from '../pkg/auth/jwt.auth';
-import initDB from '../pkg/database/database';
 import Handler from '../internal/delivery/handler';
+import initDbConnection from '../pkg/database/database';
+import { JwtManager } from '../pkg/auth/jwt.auth';
 import { S3Storage } from '../pkg/storage/s3';
 import { ClientOTP } from '../pkg/otp/twilio';
 
-export const runApp = () => {
-  const configs = initConfigs();
+const configs = initConfigs();
+const dataSource = initDbConnection(
+  configs.db.DB_HOST,
+  configs.db.DB_PORT,
+  configs.db.DB_USER,
+  configs.db.DB_PASSWORD,
+  configs.db.DB_NAME,
+);
 
-  const ds = initDB(configs.db.DB_HOST, configs.db.DB_PORT, configs.db.DB_USER, configs.db.DB_PASSWORD, configs.db.DB_NAME);
-  const jwtManager = new JwtManager(configs.jwt.JWT_SIGNING_KEY, configs.jwt.JWT_TTL);
-  const s3 = new S3Storage(configs.s3.S3_REGION, configs.s3.S3_BUCKET);
-  const otp = new ClientOTP(configs.otp.TWILIO_SID, configs.otp.TWILIO_TOKEN, configs.otp.TWILIO_SERVICE);
+const createApp = async (dsPromise: Promise<DataSource>, cfgs: Configs) => {
+  const ds = await dsPromise;
+  const jwtManager = new JwtManager(cfgs.jwt.JWT_SIGNING_KEY, cfgs.jwt.JWT_TTL);
+  const s3 = new S3Storage(cfgs.s3.S3_REGION, cfgs.s3.S3_BUCKET);
+  const otp = new ClientOTP(cfgs.otp.TWILIO_SID, cfgs.otp.TWILIO_TOKEN, cfgs.otp.TWILIO_SERVICE);
   const deps = new Deps(
     new Repositories(ds),
     jwtManager,
@@ -22,6 +30,9 @@ export const runApp = () => {
   );
   const services = new Services(deps);
   const handler = new Handler(services, jwtManager);
+  const app = handler.initHandler();
 
-  return handler.initHandler();
+  return app;
 };
+
+export default createApp(dataSource, configs);
