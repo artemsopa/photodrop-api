9cf1d64dd9fd832aa8503717e67ac49256af0ac8
Artom Sopun <artomsopun@gmail.com>
2022-09-05 17:28:20 +0300

    Add otp client initialization

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,7 @@ import { JwtManager } from '../pkg/auth/jwt.auth';
 import initDB from '../pkg/database/database';
 import Handler from '../internal/delivery/handler';
 import { S3Storage } from '../pkg/storage/s3';
+import { ClientOTP } from '../pkg/otp/twilio';
 
 export const runApp = () => {
   const configs = initConfigs();
@@ -12,10 +13,12 @@ export const runApp = () => {
   const ds = initDB(configs.db.DB_HOST, configs.db.DB_PORT, configs.db.DB_USER, configs.db.DB_PASSWORD, configs.db.DB_NAME);
   const jwtManager = new JwtManager(configs.jwt.JWT_SIGNING_KEY, configs.jwt.JWT_TTL);
   const s3 = new S3Storage(configs.s3.S3_REGION, configs.s3.S3_BUCKET);
+  const otp = new ClientOTP(configs.otp.TWILIO_SID, configs.otp.TWILIO_TOKEN, configs.otp.TWILIO_SERVICE);
   const deps = new Deps(
     new Repositories(ds),
     jwtManager,
     s3,
+    otp,
   );
   const services = new Services(deps);
   const handler = new Handler(services, jwtManager);
