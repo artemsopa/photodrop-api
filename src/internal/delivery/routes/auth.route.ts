b7d2539b3f1716292cd098b312df8e5a6e136f5b
Artom Sopun <artomsopun@gmail.com>
2022-09-05 17:26:16 +0300

    Add send code, verify user routes

--- src/internal/delivery/routes/auth.route.ts
+++ src/internal/delivery/routes/auth.route.ts
@@ -2,7 +2,7 @@ import {
   NextFunction, Request, Response, Router,
 } from 'express';
 import { IAuthService } from '../../service/service';
-import { loginSchema } from './joi-schemas/auth.schema';
+import { loginSchema, phoneSchema, verifySchema } from './joi-schemas/auth.schema';
 import validateSchema from './joi-schemas/schema';
 
 class AuthRoute {
@@ -12,7 +12,9 @@ class AuthRoute {
 
   initRoutes() {
     return Router()
-      .post('/login', this.login.bind(this));
+      .post('/login', this.login.bind(this))
+      .post('/send-code', this.getVerificationCode.bind(this))
+      .post('/verify-user', this.verifyUser.bind(this));
   }
 
   private async login(req: Request, res: Response, next: NextFunction) {
@@ -20,6 +22,26 @@ class AuthRoute {
       const body = validateSchema(loginSchema, req.body);
       const token = await this.authService.signIn(body.login, body.password);
       res.status(200).json({ token });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  private async getVerificationCode(req: Request, res: Response, next: NextFunction) {
+    try {
+      const body = validateSchema(phoneSchema, req.body);
+      await this.authService.getVerificationCode(body.phone);
+      res.status(200).json({ message: `Verification code successfully sent to ${body.phone}.` });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  private async verifyUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const body = validateSchema(verifySchema, req.body);
+      const message = await this.authService.verifyUser(body.phone, body.code);
+      res.status(200).json({ message });
     } catch (error) {
       next(error);
     }
