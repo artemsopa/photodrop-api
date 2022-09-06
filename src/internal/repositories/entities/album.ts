b481247efce55f26f82ddd22318aa133e12f6075
Artom Sopun <artomsopun@gmail.com>
2022-09-07 02:55:50 +0300

    Add phgraphId

---
+++ src/internal/repositories/entities/album.ts
@@ -0,0 +1,36 @@
+import {
+  Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany,
+} from 'typeorm';
+import Photo from './photo';
+import Phgraph from './phgraph';
+
+@Entity({ name: 'albums' })
+export default class Album {
+  @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+  @Column()
+    title: string;
+
+  @Column()
+    location: string;
+
+  @Column({ name: 'date' })
+    date: number;
+
+  @Column({ name: 'phgraph_id' })
+    phgraphId: string;
+  @ManyToOne(() => Phgraph, phgraph => phgraph.albums, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'phgraph_id' })
+    phgraph: Phgraph;
+
+  @OneToMany(() => Photo, photo => photo.album)
+    photos: Photo[];
+
+  constructor(title: string, location: string, date: number, phgraphId: string) {
+    this.title = title;
+    this.location = location;
+    this.date = date;
+    this.phgraphId = phgraphId;
+  }
+}
