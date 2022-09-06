6b6eb37b88af471cdc1f65d2d9d3dd60f1289755
Artom Sopun <artomsopun@gmail.com>
2022-09-06 21:05:40 +0300

    Delete all camerist methods except getByLogin

--- src/internal/repository/camerists.repo.ts
+++ src/internal/repository/camerists.repo.ts
@@ -9,34 +9,8 @@ class CameristsRepo implements ICameristsRepo {
     this.repo = ds.getRepository(Camerist);
   }
 
-  async getAll(id: string): Promise<Camerist[]> {
-    return await this.repo.find({ where: { id: Not(id) } });
-  }
-
-  async isLoginExists(login: string): Promise<boolean> {
-    const camerist = await this.repo.findOne({
-      where: {
-        login,
-      },
-    });
-    return !!camerist;
-  }
-
-  async isEmailExists(email: string): Promise<boolean> {
-    const camerist = await this.repo.findOne({
-      where: {
-        email,
-      },
-    });
-    return !!camerist;
-  }
-
   async getByLogin(login: string): Promise<Camerist | null> {
     return await this.repo.findOne({ where: { login } });
-  }
-
-  async create(camerist: Camerist): Promise<void> {
-    await this.repo.save(camerist);
   }
 }
 
--- src/internal/repository/repository.ts
+++ src/internal/repository/repository.ts
@@ -7,15 +7,10 @@ import AlbumsRepo from './albums.repo';
 import PhotosRepo from './photos.repo';
 import User from './entities/user';
 import UsersRepo from './users.repo';
-import Order from './entities/order';
 import OrdersRepo from './orders.repo';
 
 export interface ICameristsRepo {
-  getAll(id: string): Promise<Camerist[]>;
-  isLoginExists(login: string): Promise<boolean>
-  isEmailExists(email: string): Promise<boolean>;
   getByLogin(login: string): Promise<Camerist | null>;
-  create(camerist: Camerist): Promise<void>;
 }
 
 export interface IUsersRepo {
