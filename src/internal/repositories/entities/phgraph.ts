1a9e63185fbad39aaf3f13c66713617ba395c124
Artom Sopun <artomsopun@gmail.com>
2022-09-07 02:56:38 +0300

    Update table-name

---
+++ src/internal/repositories/entities/phgraph.ts
@@ -0,0 +1,36 @@
+import {
+  Entity, PrimaryGeneratedColumn, Column, OneToMany,
+} from 'typeorm';
+import Album from './album';
+import Photo from './photo';
+
+@Entity({ name: 'phgraphs' })
+export default class Phgraph {
+  @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+  @Column({ unique: true })
+    login: string;
+
+  @Column()
+    password: string;
+
+  @Column({ type: String, nullable: true, name: 'full_name' })
+    fullName: string | null;
+
+  @Column({ type: String, nullable: true, unique: true })
+    email: string | null;
+
+  @OneToMany(() => Album, album => album.phgraph)
+    albums: Album[];
+
+  @OneToMany(() => Photo, photo => photo.phgraph)
+    photos: Photo[];
+
+  constructor(login: string, password: string, fullName: string | null, email: string | null) {
+    this.login = login;
+    this.password = password;
+    this.fullName = fullName;
+    this.email = email;
+  }
+}
