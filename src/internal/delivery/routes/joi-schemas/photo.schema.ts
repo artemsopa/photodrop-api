b207b5b81b9f31660961bfd7c0733675f358875b
Artom Sopun <artomsopun@gmail.com>
2022-09-07 03:17:43 +0300

    Update insert photo schema

--- src/internal/delivery/routes/joi-schemas/photo.schema.ts
+++ src/internal/delivery/routes/joi-schemas/photo.schema.ts
@@ -5,13 +5,12 @@ export const getSignedUrlSchema = Joi.object({
   contentType: Joi.string().required(),
 });
 
-const photoKey = Joi.string().required();
+const photoSchema = Joi.object({
+  key: Joi.string().required(),
+  userId: Joi.string().required(),
+});
 
 export const insertPhotosSchema = Joi.object({
   albumId: Joi.string().required(),
-  photos: Joi.array().items(photoKey),
+  photos: Joi.array().items(photoSchema),
 });
-
-export const idReqSchema = Joi.object().keys({
-  id: Joi.string().required(),
-}).unknown(true);
