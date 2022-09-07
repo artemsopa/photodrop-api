d96aa01ba6d8613e309442ac6db6f583d2784944
Artom Sopun <artomsopun@gmail.com>
2022-09-07 03:09:31 +0300

    Update function result

--- src/internal/delivery/middlewares/notf.middleware.ts
+++ src/internal/delivery/middlewares/notf.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
 
-const notfMiddleware = (req: Request, res: Response) => res.status(404).json({ message: 'Not found!' });
+const notfMiddleware = (req: Request, res: Response) => res.status(404).json({ message: 'Not Found!' });
 
 export default notfMiddleware;
