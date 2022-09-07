5e84c9805936b09805369d59563dca70dbe82653
Artom Sopun <artomsopun@gmail.com>
2022-09-07 03:22:27 +0300

    Update routes imports

--- src/internal/service/dtos/order.ts
+++
@@ -1,8 +0,0 @@
-export class OrderInput {
-  photoId: string;
-  userIds: string[];
-  constructor(photoId: string, userIds: string[]) {
-    this.photoId = photoId;
-    this.userIds = userIds;
-  }
-}
