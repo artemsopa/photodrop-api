24f73828e6e08c0c77fd06e8f20cb226d6bd536f
Artom Sopun <artomsopun@gmail.com>
2022-09-07 03:18:12 +0300

    Update context

--- src/internal/delivery/routes/joi-schemas/schema.ts
+++ src/internal/delivery/routes/joi-schemas/schema.ts
@@ -1,8 +1,8 @@
 import Joi from 'joi';
 import ApiError from '../../../domain/error';
 
-const validateSchema = (schema: Joi.Schema, ctx: any) => {
-  const { error, value } = schema.validate(ctx, { abortEarly: false });
+const validateSchema = (schema: Joi.Schema, context: any) => {
+  const { error, value } = schema.validate(context, { abortEarly: false });
   if (error) throw new ApiError(400, `Bad Request! ${error.details[0].message}`);
   return value;
 };
