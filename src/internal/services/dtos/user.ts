3432a8b12d619ae3a4cd62c0da6d7269660a2cba
Artom Sopun <artomsopun@gmail.com>
2022-09-10 21:22:54 +0300

    Remove id from user info class

--- src/internal/services/dtos/user.ts
+++ src/internal/services/dtos/user.ts
@@ -1,11 +1,9 @@
 export class UserInfo {
-  id: string;
   phone: string;
   fullName: string | null;
   email: string | null;
   avatar: string | null;
-  constructor(id: string, phone: string, fullName: string | null, email: string | null, avatar: string | null) {
-    this.id = id;
+  constructor(phone: string, fullName: string | null, email: string | null, avatar: string | null) {
     this.phone = phone;
     this.fullName = fullName;
     this.email = email;
