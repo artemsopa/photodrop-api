fd4f54e712b0d6ff040af00e1d919b91dc03197a
Artom Sopun <artomsopun@gmail.com>
2022-09-15 23:31:55 +0300

    Update app creating

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,38 +1,26 @@
-import { DataSource } from 'typeorm';
 import initConfigs, { Configs } from '../configs/config';
 import Repositories from '../internal/repositories/repositories';
 import Services, { Deps } from '../internal/services/services';
 import Handler from '../internal/delivery/handler';
 import initDbConnection from '../pkg/database/database';
+import TwilioOTP from '../pkg/otp/twilio.otp';
 import { JwtManager } from '../pkg/auth/jwt.auth';
 import { S3Storage } from '../pkg/storage/s3';
-import { ClientOTP } from '../pkg/otp/twilio';
 
-const configs = initConfigs();
-const dataSource = initDbConnection(
-  configs.db.DB_HOST,
-  configs.db.DB_PORT,
-  configs.db.DB_USER,
-  configs.db.DB_PASSWORD,
-  configs.db.DB_NAME,
-);
+const createApp = async () => {
+  const cfgs = initConfigs();
+  const ds = initDbConnection(cfgs.db.DB_HOST, cfgs.db.DB_PORT, cfgs.db.DB_USER, cfgs.db.DB_PASSWORD, cfgs.db.DB_NAME);
 
-const createApp = async (dsPromise: Promise<DataSource>, cfgs: Configs) => {
-  const ds = await dsPromise;
   const jwtManager = new JwtManager(cfgs.jwt.JWT_SIGNING_KEY, cfgs.jwt.JWT_TTL);
   const s3 = new S3Storage(cfgs.s3.S3_REGION, cfgs.s3.S3_BUCKET);
-  const otp = new ClientOTP(cfgs.otp.TWILIO_SID, cfgs.otp.TWILIO_TOKEN, cfgs.otp.TWILIO_SERVICE);
-  const deps = new Deps(
-    new Repositories(ds),
-    jwtManager,
-    s3,
-    otp,
-  );
+  const otp = new TwilioOTP(cfgs.otp.TWILIO_SID, cfgs.otp.TWILIO_TOKEN, cfgs.otp.TWILIO_SERVICE);
+
+  const deps = new Deps(new Repositories(await ds), jwtManager, s3, otp);
   const services = new Services(deps);
   const handler = new Handler(services, jwtManager);
-  const app = handler.initHandler();
 
+  const app = handler.initHandler();
   return app;
 };
 
-export default createApp(dataSource, configs);
+export default createApp();
