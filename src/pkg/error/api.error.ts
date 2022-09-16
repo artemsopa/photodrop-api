c559bf97b6250ec7b1fe01d91119d0d6abf5fc6f
Artom Sopun <artomsopun@gmail.com>
2022-09-16 15:23:24 +0300

    Move api error to pkg

---
+++ src/pkg/error/api.error.ts
@@ -0,0 +1,12 @@
+export default class ApiError extends Error {
+  status: number;
+  errors: any[];
+
+  constructor(status: number, message: string, errors: any[] = []) {
+    super(message);
+    this.status = status;
+    this.errors = errors;
+  }
+
+  static unauthorized = () => new ApiError(401, 'You are unauthorized!');
+}
