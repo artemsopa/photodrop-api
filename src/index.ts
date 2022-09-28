f4b6690c84923e5ba8d9f50288148bb9c7e80a12
Artom Sopun <artomsopun@gmail.com>
2022-09-28 18:05:34 +0300

    Change waiting for empty event loop to false

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import serverless, { Handler } from 'serverless-http';
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import serverless from 'serverless-http';
 import app from './app/app';
 
-export const handler: Handler = async (event, context) => serverless(await app)(event, context);
+export const handler = async (event: APIGatewayEvent, context: Context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return serverless(await app)(event, context);
+};
