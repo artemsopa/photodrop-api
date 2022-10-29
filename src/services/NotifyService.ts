af27957d87f92fce5f49bf6352fa4919dd989112
Artom Sopun <artomsopun@gmail.com>
2022-10-29 08:19:25 +0300

    Add notify service

---
+++ src/services/NotifyService.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+import { Otp } from '@/utils/Otp';
+import { Album } from '@/entities/Album';
+import { Message } from '@/dtos/message';
+
+export class NotifyService {
+  constructor(private readonly ds: DataSource, private readonly otp: Otp) {
+    this.ds = ds;
+    this.otp = otp;
+  }
+
+  public notificatePhotosUploaded = async (message: Message) => {
+    if (!this.ds.manager.connection.isInitialized) await this.ds.initialize();
+
+    const album = await this.ds.getRepository(Album).findOneBy({ id: message.albumId });
+    if (!album) throw new Error('Unknown album');
+
+    message.phones.forEach(async (phone) => await this.otp.sendSms(
+      phone,
+      `Find new photos at "${album.title}" album!`,
+    ));
+  };
+}
