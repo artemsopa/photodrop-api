50f27668640a71aa54a0be400928af6d8e12bbd6
Artom Sopun <artomsopun@gmail.com>
2022-11-01 20:43:56 +0200

    Update verify exception

--- src/utils/Jwt.ts
+++ src/utils/Jwt.ts
@@ -1,4 +1,5 @@
 import { JwtPayload, sign, verify } from 'jsonwebtoken';
+import { ApiError } from '@/utils/ApiError';
 
 export class Jwt {
   constructor(private readonly signingKey: string, private readonly ttl: string) {
@@ -8,8 +9,12 @@ export class Jwt {
 
   public newToken = (key: string) => sign({ key }, this.signingKey, { expiresIn: this.ttl });
 
-  public verifyToken(token: string) {
-    const payload = verify(token, this.signingKey) as JwtPayload;
-    return payload.key;
-  }
+  public verifyToken = (token: string) => {
+    try {
+      const payload = verify(token, this.signingKey) as JwtPayload;
+      return payload.key;
+    } catch (error) {
+      throw ApiError.unauthorized('Jwt token expired');
+    }
+  };
 }
